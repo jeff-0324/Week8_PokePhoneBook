78bf909ff428bd4e5ba8ec147c01653bc26021e6
jeff-0324 <jeffap324@gmail.com>
2024-12-09 19:41:01 +0900

    📰 new : coreData 파일 생성

---
+++ pokePhoneBook/PhoneBook+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PhoneBook+CoreDataClass.swift
+//  pokePhoneBook
+//
+//  Created by jae hoon lee on 12/9/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PhoneBook)
+public class PhoneBook: NSManagedObject {
+
+}
